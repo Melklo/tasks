6d877f9a3ea7adfd376ab469b8d6d71c52335efc
Melklo <86657154+Melklo@users.noreply.github.com>
2023-01-22 11:29:53 +0300

    Add files via upload

--- tasks/task_3_classes.py
+++ tasks/task_3_classes.py
@@ -25,48 +25,169 @@
 
 
 class LengthUnits:
-    ...
+
+    def __init__(self, value, lehgth_units='метр', value_basic=1):
+        self.value = value
+        self.lehgth_units = lehgth_units
+        self.value_basic = value_basic
+
+    def __eq__(self, other):
+        if isinstance(other, LengthUnits):
+            return self.value_basic == other.value_basic
+        return False
+
+    def __lt__(self, other):
+        return self.value_basic < other.value_basic
+
+    def __add__(self, other):
+        return f"{self.value_basic + other.value_basic} метров "
+
+    def __iadd__(self, other):
+        self.value_basic = self.value_basic + other.value_basic
+        return f"{self.value_basic} метров"
+
+    def __sub__(self, other):
+        return LengthUnits(self.value_basic - other.value_basic, self.lehgth_units)
+
+    def __isub__(self, other):
+        self.value_basic = self.value_basic - other.value_basic
+        return self.value_basic
+
+    def __mul__(self, other):
+        return LengthUnits(self.value_basic * other.value_basic, self.lehgth_units)
+
+    def __imul__(self, other):
+        self.value_basic = self.value_basic * other.value_basic
+        return self.value_basic
+
+    def __divmod__(self, other):
+        self.value_basic = self.value_basic / other.value_basic
+        return self.value_basic
+
+    def __idiv__(self, other):
+        self.value_basic = self.value_basic / other.value_basic
+        return self.value_basic
+
+    def __str__(self):
+        return f"{self.value} {self.lehgth_units}"
 
 
-class Millimeters:
-    ...
+class Millimeters(LengthUnits):
+    def __init__(self, value, value_basic=1, lehgth_units='миллиметров'):
+        super(Millimeters, self).__init__(value_basic)
+        self.value = value
+        self.lehgth_units = lehgth_units
+        self.value_basic = self.value / 1000
+
+    def __str__(self):
+        return f"{self.value} {self.lehgth_units}"
 
 
-class Centimeters:
-    ...
+class Centimeters(LengthUnits):
+    def __init__(self, value, value_basic=1, lehgth_units='сантиметров'):
+        super(Centimeters, self).__init__(value_basic)
+        self.value = value
+        self.lehgth_units = lehgth_units
+        self.value_basic = self.value / 100
+
+    def __str__(self):
+        return f"{self.value} {self.lehgth_units}"
 
 
-class Meters:
-    ...
+class Meters(LengthUnits):
+    def __init__(self, value, value_basic=1, lehgth_units='метров'):
+        super(Meters, self).__init__(value_basic)
+        self.value = value
+        self.lehgth_units = lehgth_units
+        self.value_basic = self.value / 1
+
+    def __str__(self):
+        return f"{self.value} {self.lehgth_units}"
 
 
-class Kilometers:
-    ...
+class Kilometers(LengthUnits):
+    def __init__(self, value, value_basic=1, lehgth_units='километров'):
+        super(Kilometers, self).__init__(value_basic)
+        self.value = value
+        self.lehgth_units = lehgth_units
+        self.value_basic = self.value * 1000
+
+    def __str__(self):
+        return f"{self.value} {self.lehgth_units}"
 
 
-class Inches:
-    ...
+class Inches(LengthUnits):
+    def __init__(self, value, value_basic=1, lehgth_units='дюймов'):
+        super(Inches, self).__init__(value_basic)
+        self.value = value
+        self.lehgth_units = lehgth_units
+        self.value_basic = self.value * 0.0254
+
+    def __str__(self):
+        return f"{self.value} {self.lehgth_units}"
 
 
-class Feets:
-    ...
+class Feets(LengthUnits):
+    def __init__(self, value, value_basic=1, lehgth_units='футов'):
+        super(Feets, self).__init__(value_basic)
+        self.value = value
+        self.lehgth_units = lehgth_units
+        self.value_basic = self.value * 0.3
+
+    def __str__(self):
+        return f"{self.value} {self.lehgth_units}"
 
 
-class Yards:
-    ...
+class Yards(LengthUnits):
+    def __init__(self, value, value_basic=1, lehgth_units='ярдов'):
+        super(Yards, self).__init__(value_basic)
+        self.value = value
+        self.lehgth_units = lehgth_units
+        self.value_basic = self.value * 0.91
+
+    def __str__(self):
+        return f"{self.value} {self.lehgth_units}"
 
 
-class Miles:
-    ...
+class Miles(LengthUnits):
+    def __init__(self, value, value_basic=1, lehgth_units='миль'):
+        super(Miles, self).__init__(value_basic)
+        self.value = value
+        self.lehgth_units = lehgth_units
+        self.value_basic = self.value * 1.61
+
+    def __str__(self):
+        return f"{self.value} {self.lehgth_units}"
 
 
-class Fen:
-    ...
+class Fen(LengthUnits):
+    def __init__(self, value, value_basic=1, lehgth_units='фэн'):
+        super(Fen, self).__init__(value_basic)
+        self.value = value
+        self.lehgth_units = lehgth_units
+        self.value_basic = self.value / 10000
+
+    def __str__(self):
+        return f"{self.value} {self.lehgth_units}"
 
 
-class Chi:
-    ...
+class Chi(LengthUnits):
+    def __init__(self, value, value_basic=1, lehgth_units='чи'):
+        super(Chi, self).__init__(value_basic)
+        self.value = value
+        self.lehgth_units = lehgth_units
+        self.value_basic = self.value * 0.32
+
+    def __str__(self):
+        return f"{self.value} {self.lehgth_units}"
 
 
-class In:
-    ...
+class In(LengthUnits):
+    def __init__(self, value, value_basic=1, lehgth_units='ин'):
+        super(In, self).__init__(value_basic)
+        self.value = value
+        self.lehgth_units = lehgth_units
+        self.value_basic = self.value * 0.0254
+
+    def __str__(self):
+        return f"{self.value} {self.lehgth_units}"
